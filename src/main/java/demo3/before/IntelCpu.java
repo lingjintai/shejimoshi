6453b1a36616b661d3b269ecb2a7b9b5fe2c01c8
ljt <ljt@2020>
2021-07-29 14:02:02 +0800

    1111

---
+++ src/main/java/demo3/before/IntelCpu.java
@@ -0,0 +1,14 @@
+package demo3.before;
+
+/**
+ * @description: intel cpu
+ * @author: ljt
+ * @time: 2021/7/28 0028 15:03
+ */
+public class IntelCpu {
+
+   public void run(){
+       System.out.println("使用了intel处理器");
+   }
+
+}

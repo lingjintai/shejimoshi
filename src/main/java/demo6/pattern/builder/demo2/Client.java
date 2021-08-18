b3e475cf5069e06d50c71bb13689f18ca363aea0
ljt <ljt@2020>
2021-08-18 09:32:41 +0800

    20210730

---
+++ src/main/java/demo6/pattern/builder/demo2/Client.java
@@ -0,0 +1,20 @@
+package demo6.pattern.builder.demo2;
+
+/**
+ * @description:
+ * @author: ljt
+ * @time: 2021/8/17 0017 10:34
+ */
+public class Client {
+    public static void main(String[] args) {
+        new Phone.Builder()
+                .cpu("intel")
+                .screen("三星")
+                .memory("内存条")
+                .mainboard("主板")
+                .build();
+    }
+
+}
+
+

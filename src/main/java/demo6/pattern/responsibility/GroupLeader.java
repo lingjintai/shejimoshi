9208c741c643e61cd8d8f364ac655f3dbdcab3c0
ljt <ljt@2020>
2021-10-12 18:20:57 +0800

    职责链模式

---
+++ src/main/java/demo6/pattern/responsibility/GroupLeader.java
@@ -0,0 +1,21 @@
+package demo6.pattern.responsibility;
+
+/**
+ * @description: 小组长类  具体的处理者
+ * @author: ljt
+ * @time: 2021/10/12 0012 16:17
+ */
+public class GroupLeader extends Handler {
+
+    public GroupLeader() {
+        super(0, Handler.NUM_ONE);
+    }
+
+    @Override
+    protected void handlerLevel(LeaveRequest request) {
+        System.out.println(request.getName() +
+                "请假" + request.getNum() + "天" +
+                request.getContent());
+        System.out.println("小组长审批 同意");
+    }
+}
---
+++ src/main/java/demo6/pattern/responsibility/LeaveRequest.java
@@ -0,0 +1,37 @@
+package demo6.pattern.responsibility;
+
+/**
+ * @description:  请假条类
+ * @author: ljt
+ * @time: 2021/10/12 0012 15:39
+ */
+public class LeaveRequest {
+
+    //请假人姓名
+    private  String name;
+
+    //请假天数
+    private  int num;
+
+    //请假内容
+    private String content;
+
+    public LeaveRequest(String name, int num, String content) {
+        this.name = name;
+        this.num = num;
+        this.content = content;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
